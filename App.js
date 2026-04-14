8abcbcc98a33fa746173126d10285ea349266f13
Austinspeed <austinikedinma@gmail.com>
2026-04-15 00:57:39 +0100

    Finish the app, added AsyncStorage to store token on device to remain logged in when we exit the app

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { StatusBar } from "expo-status-bar";
@@ -9,6 +9,11 @@ import WelcomeScreen from "./screens/WelcomeScreen";
 import { Colors } from "./constants/styles";
 import AuthContextProvider, { AuthContext } from "./store/auth-context";
 import IconButton from "./components/ui/IconButton";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import * as SplashScreen from 'expo-splash-screen';
+
+
+SplashScreen.preventAutoHideAsync();
 
 const Stack = createNativeStackNavigator();
 
@@ -66,12 +71,37 @@ const Navigation = () => {
   );
 };
 
+const Root = () => {
+  const authCtx = useContext(AuthContext);
+  const [isTryingLogin, setIsTryingLogin] = useState(true)
+
+  useEffect(() => {
+    async function fetchToken() {
+      const storedToken = await AsyncStorage.getItem('token')
+
+      if (storedToken) {
+        authCtx.authenticate(storedToken)
+      }
+
+      setIsTryingLogin(false)
+      await SplashScreen.hideAsync(); 
+    }
+
+    fetchToken()
+  }, [])
+
+  if (isTryingLogin) {
+    return null
+  }
+  return <Navigation />
+}
+
 const App = () => {
   return (
     <>
       <StatusBar style="light" />
       <AuthContextProvider>
-        <Navigation />
+        <Root />
       </AuthContextProvider>
     </>
   );
